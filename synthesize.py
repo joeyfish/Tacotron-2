f085bb28679b9a3a1426d8c54bfb1a3c4c2d9067
begeekmyfriend <begeekmyfriend@gmail.com>
2019-10-25 21:26:28 +0800

    Add eos behind each eval text
    Signed-off-by: begeekmyfriend <begeekmyfriend@gmail.com>

--- synthesize.py
+++ synthesize.py
@@ -31,7 +31,7 @@ def get_sentences(args):
 		sentences = list(map(lambda l: l.strip(), lines[1::2]))
 	else:
 		sentences = hparams.sentences
-	return list(map(lambda s: s + _eos, sentences))
+	return list(map(lambda s: s + ' .' + _eos, sentences))
 
 
 def main():
